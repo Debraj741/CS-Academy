d62f72b3e786744bc689ccdd3315a598907415e7
Debraj Kundu <debrajkundu657@gmail.com>
2024-10-19 14:06:11 +0530

    Add Picture in Why Choose Us Section

--- src/components/WhyChooseUs.tsx
+++ src/components/WhyChooseUs.tsx
@@ -1,24 +1,37 @@
 "use client";
 import React from "react";
 import { StickyScroll } from "./ui/sticky-scroll-reveal";
+import Image from "next/image";
 
 const content = [
   {
     title: "Expert-Led Courses",
     description: "Learn from industry professionals with years of experience in computer science. Our expert instructors provide in-depth knowledge and hands-on guidance to help you master the latest technologies.",
     content: (
-      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white font-extrabold">
-        Expert-Led Courses
-      </div>
+      <div className="h-full w-full  flex items-center justify-center text-white">
+      <Image
+        src="https://plus.unsplash.com/premium_photo-1678565202049-9e37b2da8060?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8fDB8fHww"
+        width={300}
+        height={300}
+        className="h-full w-full object-cover"
+        alt="linear board demo"
+      />
+    </div>
     ),
   },
   {
     title: "Real-World Projects",
     description: "Apply your learning to real-world problems through practical projects. Gain hands-on experience that prepares you for challenges in the tech industry and boosts your confidence in building complex systems.",
     content: (
-      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white font-extrabold">
-        Real-World Projects
-      </div>
+      <div className="h-full w-full  flex items-center justify-center text-white">
+      <Image
+        src="https://plus.unsplash.com/premium_photo-1661293879952-c5c093282801?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHByb2plY3RzfGVufDB8fDB8fHww"
+        width={300}
+        height={300}
+        className="h-full w-full object-cover"
+        alt="linear board demo"
+      />
+    </div>
     ),
   },
   {
